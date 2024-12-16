0528e5e9e545173b7d769cc9c11bde788dbe30de
Sudhanva Acharya <sudhanva.udupi55@gmail.com>
2024-12-16 20:01:19 +0530

    Add Utils directory with keyboard shortcuts

---
+++ DropBeat/DropBeat/Utils/Shortcuts.swift
@@ -0,0 +1,5 @@
+import KeyboardShortcuts
+
+extension KeyboardShortcuts.Name {
+    static let toggleCommandPalette = Self("toggleCommandPalette", default: .init(.space, modifiers: [.command, .option]))
+} 
